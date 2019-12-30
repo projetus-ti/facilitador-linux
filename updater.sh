f0a17d151d30d68784ea2f1b881968fc908c8a3d
Evandro Begati <begati@projetusti.com.br>
2019-12-30 18:18:19 -0300

    Implementado o script de atualizacao dos fontes via git

--- updater.sh
+++ updater.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 # Descricao: Atualizador do facilitador
 # Autor: Evandro Begati
-# Data: 28/07/2018
+# Data: 30/12/2019
 # Uso: ./updater.sh
 
-# Criar diretorios
-cd /opt/projetus/facilitador
-nohup mkdir cache >/dev/null 2>&1
+source /opt/projetus/funcoes.sh
 
-# Atualizar scripts
-nohup rm -Rf *.sh >/dev/null 2>&1
-nohup wget https://cdn.projetusti.com.br/infra/facilitador/scripts.tar.gz -q --no-check-certificate -O ./cache/scripts.tar.gz >/dev/null 2>&1
-nohup tar -xvf ./cache/scripts.tar.gz -C . >/dev/null 2>&1
-chmod +x *.sh
+# verificar se o zenity esta instalado
+if ! [ -x "$(command -v zenity)" ]; then
+  showMessage "É necessário instalar um componente visual, clique em OK para continuar."
+  executar "pkexec bash -c 'apt-get update && apt-get install zenity -y'"
+fi
 
-# Atualizar atalhos
-nohup rm -Rf /opt/projetus/facilitador/atalhos >/dev/null 2>&1
-nohup wget https://cdn.projetusti.com.br/infra/facilitador/atalhos.tar.gz --no-check-certificate -O ./cache/atalhos.tar.gz >/dev/null 2>&1
-nohup tar -xvf ./cache/atalhos.tar.gz -C /opt/projetus/facilitador >/dev/null 2>&1
-chmod +x ./atalhos/*.sh
-chmod +x ./atalhos/*.desktop
+# verificar se o git esta instalado
+if ! [ -x "$(command -v git)" ]; then
+  showMessage "É necessário instalar o git, clique em OK para continuar."
+  executar "pkexec bash -c 'apt-get update && apt-get install git -y'"
+fi
 
-# Remover lixos
-nohup rm -Rf ./cache/* >/dev/null 2>&1
+# atualizar a base de scripts 
+DIR="/opt/projetus/facilitador/.git"
+if [ -d "$DIR" ]; then
+  # excluir scripts antigos caso a base nao venha do git
+  rm -Rf /opt/projetus/facilitador
+  executar "git pull https://github.com/projetus-ti/facilitador-linux.git /opt/projetus/facilitador"
+else
+  # resetar os fontes de acordo com o repositorio
+  cd /opt/projetus/facilitador
+  git reset --hard HEAD
+  git pull origin master
+
+fi
 
 # Executar a aplicacao
 nohup /opt/projetus/facilitador/facilitador.sh >/dev/null 2>&1 &
