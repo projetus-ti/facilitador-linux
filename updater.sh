cc2b198c8f5ce9c906e522071a6dc7bfeb75e45b
Evandro Begati <begati@projetusti.com.br>
2020-02-24 12:11:12 -0300

    Correcao de permissoes no updater.

--- updater.sh
+++ updater.sh
@@ -12,6 +12,8 @@ git pull origin master
 # prover permissao de execucao aos scripts
 chmod -R +x /opt/projetus/facilitador/*.sh
 chmod -R +x /opt/projetus/facilitador/*.desktop
+chmod -R +x /opt/projetus/facilitador/atalhos/*.sh
+chmod -R +x /opt/projetus/facilitador/atalhos/*.desktop
 
 # Executar a aplicacao
 nohup /opt/projetus/facilitador/facilitador.sh >/dev/null 2>&1 &
