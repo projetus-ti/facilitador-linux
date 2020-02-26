59ba3a29ecaeb9f13161e53d7176e9156ff39f81
Evandro Begati <begati@projetusti.com.br>
2020-02-26 12:25:23 -0300

    fix dirf

--- atalhos/DIRF.sh
+++ atalhos/DIRF.sh
@@ -2,4 +2,4 @@
 cd "/opt/Programas RFB/Dirf2020"
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=ISO-8859-1
 umask 000
-java -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
+/usr/lib/jvm/jre1.8.0_212/bin/java -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
