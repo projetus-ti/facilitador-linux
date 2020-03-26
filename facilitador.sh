e247bfb68a1c4e80b44217ae817af1e6ee65af7c
fabianohenri <fabiano_henri@yahoo.com.br>
2020-03-26 09:43:54 -0300

    Atualização GDRAIS

--- facilitador.sh
+++ facilitador.sh
@@ -92,7 +92,7 @@ elif [ "$setor" = "Folha" ]; then ## Folha
     --height="260" --width="350"\
     --column="" --column "Programa"  --column "Descrição"\
     TRUE "ACI" "Validador do CAGED"\
-    FALSE "DIRF" "Versão 2020-1.2" \
+    FALSE "DIRF" "Versão 2020-1.2" \ 
     FALSE "GDRAIS" "Versão 2019-1.2.0" \
     FALSE "GRRF" "Versão ICP-20200128" \
     FALSE "SEFIP" "Versão 8.4-20012020" \
