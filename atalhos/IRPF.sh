1a7d340cb4fc9466675d3285797694f9a7a4190d
fabianohenri <fabiano_henri@yahoo.com.br>
2020-04-29 10:23:52 -0300

    Ajustado atalho IRPF

--- atalhos/IRPF.sh
+++ atalhos/IRPF.sh
@@ -4,4 +4,10 @@ set echo off
 
 cd ~/ProgramasRFB/IRPF2020/
 
-/usr/lib/jvm/jre1.8.0_212/bin/java -jar irpf.jar
+if [ ! -d "/usr/lib/jvm/jre1.8.0_212/bin/java" ]; then
+    executar "java -jar irpf.jar"
+  else
+    /usr/lib/jvm/jre1.8.0_212/bin/java -jar irpf.jar
+  fi
+
+#/usr/lib/jvm/jre1.8.0_212/bin/java -jar irpf.jar
