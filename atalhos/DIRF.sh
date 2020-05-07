ca9090e1221b10845eea6dc41b78a8b539226f13
fabianohenri <fabiano_henri@yahoo.com.br>
2020-05-07 10:07:47 -0300

    Correção Abertura DIRF

--- atalhos/DIRF.sh
+++ atalhos/DIRF.sh
@@ -3,7 +3,7 @@ cd "/opt/Programas RFB/Dirf2020"
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=ISO-8859-1
 umask 000
 if [ ! -d "/usr/lib/jvm/jre1.8.0_212/bin/java" ]; then
-    executar "java -jar pgdDirf.jar"
+   java -jar pgdDirf.jar
   else
     /usr/lib/jvm/jre1.8.0_212/bin/java -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
   fi
