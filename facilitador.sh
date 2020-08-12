b5da7515dfc4db4206a5f27318b4e1d636fd85f5
fabianohenri <fabiano_henri@yahoo.com.br>
2020-08-12 11:17:07 -0300

    Atualizado GIAM TO

--- facilitador.sh
+++ facilitador.sh
@@ -70,7 +70,7 @@ elif [ "$setor" = "Fiscal" ]; then ## Fiscal
     FALSE "GIA MT" "Versão 3.0.7m"\
     FALSE "GIA RS" "Versão 9-26/03/2018"\
     FALSE "GIA SP" "Momentaneamente OFF"\
-    FALSE "GIAM TO" "Versão 10.0"\
+    FALSE "GIAM TO" "Versão 10.0 2020v1"\
     FALSE "GIM ICMS PB" "Versão 2473"\
     FALSE "Livro Eletronico GDF" "Versão 2.0.9.0" \
     FALSE "SEF 2012 PE" "Versão 1.6.5"\
