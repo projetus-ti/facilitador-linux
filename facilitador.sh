fdba8d54522631421f98bc2f6026ccba996bf623
fabianohenri <fabiano_henri@yahoo.com.br>
2020-08-24 10:54:48 -0300

    update Calima Server

--- facilitador.sh
+++ facilitador.sh
@@ -117,7 +117,7 @@ elif [ "$setor" = "Projetus e Outros" ]; then ## Projetus e Outros
     --column="" --column "Programa"  --column "Descrição"\
     TRUE "Calima App - Acesso Local" "Versão 1.1.0"\
     FALSE "Calima App - Acesso Web" "Versão 1.1.0"\
-    FALSE "Calima Server" "Versão 3.5.1"\
+    FALSE "Calima Server" "Versão 3.5.2"\
     FALSE "DBeaver" "Gerenciador de Banco de Dados"\
     FALSE "Discord" "Versão 0.0.10"\
     FALSE "iSGS App" "Versão 1.0.1"\
