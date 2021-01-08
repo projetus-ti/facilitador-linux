002a6c135765254988a78274883bb17d3943f538
fabianohenri <fabiano_henri@yahoo.com.br>
2021-01-08 10:05:56 -0300

    Corrigido a regra de validação Dirf

--- atalhos/DIRF.sh
+++ atalhos/DIRF.sh
@@ -2,7 +2,7 @@
 cd "/opt/Programas RFB/Dirf2021"
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=ISO-8859-1
 umask 000
-if [ ! -d "/usr/lib/jvm/jre1.8.0_212/bin/java" ]; then
+if [ ! -e "/usr/lib/jvm/jre1.8.0_212/bin/java" ]; then
    java -jar pgdDirf.jar
   else
     /usr/lib/jvm/jre1.8.0_212/bin/java -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
