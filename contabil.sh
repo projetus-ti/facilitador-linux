7dd787f97ceb6f691215b7d1aeaf21063a49b2b8
fabianohenri <fabiano_henri@yahoo.com.br>
2021-01-15 08:48:22 -0300

    Atualização ECF 7.0 e ECD 8.0

--- contabil.sh
+++ contabil.sh
@@ -8,7 +8,7 @@ source /opt/projetus/facilitador/funcoes.sh
 acao=$1
 
 if [ "$acao" = "SPED ECD" ]; then
-  download "http://www.receita.fazenda.gov.br/publico/programas/Sped/SpedContabil/SPEDContabil_linux_x64-7.0.9.jar" "$cache_path/SPEDContabil.jar"
+  download "http://www.receita.fazenda.gov.br/publico/programas/Sped/SpedContabil/SPEDContabil_linux_x64-8.0.0.jar" "$cache_path/SPEDContabil.jar"
   executar "java -jar $cache_path/SPEDContabil.jar"
   sleep 2
   cd "$desktop_path"
@@ -23,7 +23,7 @@ if [ "$acao" = "SPED ECD" ]; then
 fi
 
 if [ "$acao" = "SPED ECF" ]; then
-  download "http://www.receita.fazenda.gov.br/publico/programas/Sped/ECF/SpedEcf_linux_x64-6.0.9.jar" "$cache_path/SpedEcf.jar"
+  download "http://www.receita.fazenda.gov.br/publico/programas/Sped/ECF/SpedEcf_linux_x64-7.0.0.jar" "$cache_path/SpedEcf.jar"
   executar "java -jar $cache_path/SpedEcf.jar"
   sleep 2
   cd "$desktop_path"
--- funcoes.sh
+++ funcoes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Setar a versao do script
-versao="4.2"
+versao="4.3"
 
 # Setar outras variaveis
 export WINEDEBUG=-all
