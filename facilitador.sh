3964c72a30481d56c6f119b655b6f5d8da5db3d8
fabianohenri <fabiano_henri@yahoo.com.br>
2021-08-06 16:52:07 -0300

    Atualização do ECF 7.0.9

--- facilitador.sh
+++ facilitador.sh
@@ -39,7 +39,7 @@ if [ "$setor" = "Contábil" ]; then ## Contabil
     --height="220" --width="350" \
     --column="" --column "Programa" --column "Descrição"\
     TRUE "SPED ECD" "Versão 8.0.8" \
-    FALSE "SPED ECF" "Versão 7.0.8" \
+    FALSE "SPED ECF" "Versão 7.0.9" \
     FALSE "Arquivo Remessa CX" "Versão V2.2.2"\
     FALSE "Receita Net BX" "Versão 25062020");
 
