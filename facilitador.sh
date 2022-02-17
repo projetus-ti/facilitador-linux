226f7c272b26b9d69cc4f20ee9aa0cfb2c05ae21
fabianohenri <fabiano_henri@yahoo.com.br>
2022-02-17 15:38:00 -0300

    Update Sped ICMS

--- facilitador.sh
+++ facilitador.sh
@@ -78,7 +78,7 @@ elif [ "$setor" = "Fiscal" ]; then ## Fiscal
     FALSE "SEF 2012 PE" "Versão 1.6.5"\
     FALSE "SEFAZNET PE" "Versão 1.24.0.3"\
     FALSE "SINTEGRA" "Versão 2017"\
-    FALSE "SPED ICMS IPI" "Versão 2.8.1");  
+    FALSE "SPED ICMS IPI" "Versão 2.8.2");  
 
     if [ $? = 1 ] ; then
       /opt/projetus/facilitador/facilitador.sh
