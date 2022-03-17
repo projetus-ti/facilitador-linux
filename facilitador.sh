cb3ad195b9bc32009d603c12cdeb72ddabc222dd
fabianohenri <fabiano_henri@yahoo.com.br>
2022-03-17 16:25:29 -0300

    Atualização da IRPF.

--- facilitador.sh
+++ facilitador.sh
@@ -128,7 +128,7 @@ elif [ "$setor" = "Projetus e Outros" ]; then ## Projetus e Outros
     FALSE "DBeaver" "Gerenciador de Banco de Dados"\
     FALSE "Discord" "Versão 0.0.16"\
     FALSE "iSGS App" "Versão 1.0.1"\
-    FALSE "IRPF" "Versão 2021 v"\
+    FALSE "IRPF" "Versão 2022 v1.1"\
     FALSE "MySuite" "Sistema de Atendimento" \
     FALSE "TeamViewer" "Versão 13" \
     FALSE "Skype" "Última Versão" \
--- funcoes.sh
+++ funcoes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Setar a versao do script
-versao="4.5.30"
+versao="4.5.31"
 
 # Setar outras variaveis
 export WINEDEBUG=-all
