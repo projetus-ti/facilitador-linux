67e2e682efa1cb7514bf2b4e258e4e58d8a5b58a
Fabiano Henrique <fabiano_henri@yahoo.com.br>
2022-04-19 11:17:12 -0300

    fix

--- atalhos/linphone.sh
+++ atalhos/linphone.sh
@@ -3,5 +3,5 @@ source /opt/projetus/facilitador/funcoes.sh
 chmod +x $atalho_path/linphone.AppImage
 chmod +x $atalho_path/linphone.desktop
 cp "$atalho_path/linphone.desktop" /home/$USER/.local/share/applications/linphone.desktop
-cp "$atalho_path/linphone.desktop" $desktop_path
+cp "$atalho_path/linphone.desktop" "$desktop_path/linphone.desktop"
 sudo apt-get purge linphone -y
