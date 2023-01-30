6bc0f01b7f540cae3f0579f12d9c3790f49f7854
Fabiano Henrique <fabiano.oliveira@projetusti.com.br>
2023-01-30 11:44:22 -0300

    Correção do atalho do sped ICMS

--- atalhos/efd-icms-ipi.sh
+++ atalhos/efd-icms-ipi.sh
@@ -4,4 +4,4 @@ set echo off
 
 cd ~/ProgramasSPED/Fiscal2/
 
-~/ProgramasSPED/Fiscal2/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/Fiscal2/fiscalpva.jar
+~/ProgramasSPED/Fiscal2/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/Fiscal/fiscalpva.jar
