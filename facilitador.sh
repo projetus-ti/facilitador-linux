4582b9a6ea5de37fa74db8efcd8f1637ddfdf569
Evandro Begati <begati@projetusti.com.br>
2023-03-29 10:42:27 -0300

    Atualizado DMED pra versão 2023.

--- facilitador.sh
+++ facilitador.sh
@@ -60,7 +60,7 @@ elif [ "$setor" = "Fiscal" ]; then ## Fiscal
     TRUE "DAPI MG" "Versão 9.03.00"\
     FALSE "DAC AL" "Versão 2.2.10.12"\
     FALSE "DCTF" "Mensal v. 3.6"\
-    FALSE "DMED" "Versão 2022"\
+    FALSE "DMED" "Versão 2023"\
     FALSE "DIEF CE" "Versão 6.0.8"\
     FALSE "DIEF MA" "Versão 6.4.5"\
     FALSE "DIEF PA" "Versão 2022.2.0"\
