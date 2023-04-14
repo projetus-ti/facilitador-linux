7327dd2616271d1e982e847b3942ee0e4eef3d0f
Fabiano Henrique <fabiano.oliveira@projetusti.com.br>
2023-04-14 15:38:32 -0300

    Correção do endereço sped

--- atalhos/efd-icms-ipi.sh
+++ atalhos/efd-icms-ipi.sh
@@ -4,4 +4,4 @@ set echo off
 
 cd ~/ProgramasSPED/Fiscal/
 
-~/ProgramasSPED/Fiscal2/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/Fiscal/fiscalpva.jar
+~/ProgramasSPED/Fiscal/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/Fiscal/fiscalpva.jar
