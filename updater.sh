73c0c3b3ff499f64b52f4ef50b6977395b3d7624
Fabiano Henrique <fabiano.oliveira@projetusti.com.br>
2023-06-09 11:25:44 -0300

    Inclusão de aceitação automatica.

--- updater.sh
+++ updater.sh
@@ -18,10 +18,10 @@ chmod -R +x /opt/projetus/facilitador/atalhos/*.desktop
 # Verificar se o FlatpakWine está instalado.
 if ! (flatpak list | grep WineZGUI); then
     executar "flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo"
-    executar "flatpak --user -y install flathub org.winehq.Wine/x86_64/stable-22.08 -y"
+    executar "flatpak --user -y install flathub org.winehq.Wine/x86_64/stable-22.08"
     executar "flatpak -y remove io.github.fastrizwaan.WineZGUI"
     executar "wget -c https://github.com/fastrizwaan/flatpak-wine/releases/download/0.97.12/flatpak-winezgui_0.97.12_20230522.flatpak"
-    executar "flatpak --user install flatpak-winezgui_0.97.12_20230522.flatpak -y"
+    executar "flatpak --user install -y flatpak-winezgui_0.97.12_20230522.flatpak"
 fi
 
 # Verifica se  o Winestricks esta instalado 
