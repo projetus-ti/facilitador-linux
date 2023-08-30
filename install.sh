108f9ded30001ff1c0bf7732654c0ecc2dc45ddf
fabianohenri <fabiano_henri@yahoo.com.br>
2023-08-30 14:50:33 -0300

    Inclusão dos outros programas necessários.

--- install.sh
+++ install.sh
@@ -9,15 +9,29 @@ sudo -u $SUDO_USER env WINEARCH=win64 WINEPREFIX=/home/$SUDO_USER/.wine wineserv
 rm -rf /home/$SUDO_USER/.wine32
 rm -rf /home/$SUDO_USER/.wine
 
+# Add Wine repository
+mkdir -pm755 /etc/apt/keyrings
+wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+
+# Add flatpak package wine8.0
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user -y install flathub org.winehq.Wine/x86_64/stable-22.08
+flatpak -y remove io.github.fastrizwaan.WineZGUI
+wget -c https://github.com/fastrizwaan/flatpak-wine/releases/download/0.97.12/flatpak-winezgui_0.97.12_20230522.flatpak
+flatpak --user install flatpak-winezgui_0.97.12_20230522.flatpak
+
 # Install dependencies packages
-echo "Instalando dependência."
 dpkg --add-architecture i386
+apt-get purge -y wine*
 apt-get update
 apt-get install --install-recommends -y \
  zenity \
  git \
  exe-thumbnailer \
- cabextract 
+ cabextract \
+ winetricks \
+ winehq-staging
 
 # Create workspace dir
 sudo rm -Rf /opt/projetus/facilitador 
