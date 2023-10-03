9901e43fbae8d47b0cf466826703af825c214fb9
fabianohenri <fabiano_henri@yahoo.com.br>
2023-10-03 11:13:29 -0300

    Retirada a configuração do wine do MySuite

--- projetus.sh
+++ projetus.sh
@@ -38,7 +38,7 @@ fi
 
 
 if [ "$acao" = "MySuite" ]; then
-  configurarWine
+  #configurarWine
   download "https://cdn.projetusti.com.br/suporte/mysuite.msi" "$cache_path/mysuite.msi"
   nohup wine msiexec /i $cache_path/mysuite.msi /quiet > /dev/null 2>&1 &
   rm -Rf "$desktop_path/BraZip Central.lnk"
