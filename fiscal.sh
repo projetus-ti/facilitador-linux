351a0434588e9db9abee27ae01644ef667503d58
fabianohenri <fabiano_henri@yahoo.com.br>
2023-12-27 12:58:25 -0300

    Atualização SPED ICMS IPI 4.0.0

--- fiscal.sh
+++ fiscal.sh
@@ -66,7 +66,7 @@ fi
 
 if [ "$acao" = "SPED ICMS IPI" ]; then
 
-  download "https://servicos.receita.fazenda.gov.br/publico/programas/Sped/SpedFiscal/PVA_EFD_linux-3.0.7_x64.jar" "$cache_path/PVA_EFD.jar"
+  download "https://servicos.receita.fazenda.gov.br/publico/programas/Sped/SpedFiscal/PVA_EFD_linux-4.0.0_x64.jar" "$cache_path/PVA_EFD.jar"
   
   if [ ! -d "/usr/lib/jvm/jre1.8.0_212/bin/java" ]; then
     executar "java -jar $cache_path/PVA_EFD.jar"
--- funcoes.sh
+++ funcoes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Setar a versao do script
-versao="4.7.5"
+versao="4.7.6"
 
 # Setar outras variaveis
 export WINEDEBUG=-all
