06a90ec617a7c15cfdc4b90e8f8e6b55e9126942
Fabiano Henrique <fabiano.oliveira@projetusti.com.br>
2025-08-25 11:57:30 -0300

    Fix

--- fiscal.sh
+++ fiscal.sh
@@ -94,7 +94,10 @@ if [ "$acao" = "SPED ICMS IPI" ]; then
   rm -Rf EFD*
   rm -Rf Desinstalar*
 
-  cp "$user_path"/ProgramasSPED/Fiscal/EFD ICMS IPI.desktop "$desktop_path/Validadores"
+  # cp "$user_path"/ProgramasSPED/Fiscal/EFD ICMS IPI.desktop "$desktop_path/Validadores"
+
+  cp "$user_path"/ProgramasSPED/Fiscal/*.desktop "$desktop_path/Validadores"
+
   # cp /opt/projetus/facilitador/atalhos/efd-icms-ipi.desktop "$desktop_path/Validadores"
 
 
