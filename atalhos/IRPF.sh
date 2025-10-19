55bb66fd56c2780a9a5270771107b96be6403e4a
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-10-19 02:44:22 -0300

    Update IRPF.sh

--- atalhos/IRPF.sh
+++ atalhos/IRPF.sh
@@ -1,13 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set echo off
 
+# Usar array para pegar todos os possíveis caminhos
+JAVA_PATHS=(/usr/lib/jvm/*/bin/java)
+
+for JAVA in "${JAVA_PATHS[@]}"; do
+    if [[ -x "$JAVA" ]]; then
+        echo "Executável encontrado: $JAVA"
+        "$JAVA" -version
+    fi
+done
+
+
 cd ~/ProgramasRFB/IRPF2021/
 
-if [ ! -d "/usr/lib/jvm/jre1.8.0_212/bin/java" ]; then
+if [ ! -d "$JAVA" ]; then
     java -jar irpf.jar
   else
-    /usr/lib/jvm/jre1.8.0_212/bin/java -jar irpf.jar
+    $JAVA -jar irpf.jar
   fi
-
-#/usr/lib/jvm/jre1.8.0_212/bin/java -jar irpf.jar
+  
+  exit 0
+  
