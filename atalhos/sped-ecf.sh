3d5975fb527ad548acd2f7990bffee8f173751a3
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-10-19 03:04:00 -0300

    Update sped-ecf.sh

--- atalhos/sped-ecf.sh
+++ atalhos/sped-ecf.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set echo off
+# Colaboração: Fernando Souza https://github.com/tuxslack / https://www.youtube.com/@fernandosuporte
+# Data: 19/10/2025
 
-cd ~/ProgramasSPED/ECF/
+# Verifica se o diretório existe
+if [[ ! -d ~/ProgramasSPED/ECF/ ]]; then
+    echo "Diretório ~/ProgramasSPED/ECF/ não encontrado."
+    exit 1
+fi
 
-~/ProgramasSPED/ECF/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/ECF/irpjpva.jar
+# Verifica se o Java está presente
+if [[ ! -x ~/ProgramasSPED/ECF/jre/bin/java ]]; then
+    echo "Java não encontrado em ~/ProgramasSPED/ECF/jre/bin/java"
+    exit 1
+fi
+
+# Executa
+~/ProgramasSPED/ECF/jre/bin/java \
+    -Xmx2048m \
+    -Dfile.encoding=ISO-8859-1 \
+    -jar ~/ProgramasSPED/ECF/irpjpva.jar
+
