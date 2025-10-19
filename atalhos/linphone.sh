7181f94ac7dccf355e2c119184164b2bc37abcb1
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-10-19 02:54:54 -0300

    Update linphone.sh

--- atalhos/linphone.sh
+++ atalhos/linphone.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# Colaboração: Fernando Souza https://github.com/tuxslack / https://www.youtube.com/@fernandosuporte
+# Data: 19/10/2025
+
+ls /opt/projetus/ || exit
+
 source /opt/projetus/facilitador/funcoes.sh
+
 chmod +x $atalho_path/linphone.AppImage
 chmod +x $atalho_path/linphone.desktop
 cp "$atalho_path/linphone.desktop" /home/$USER/.local/share/applications/linphone.desktop
 cp "$atalho_path/linphone.desktop" "$desktop_path/linphone.desktop"
-sudo apt-get purge linphone -y
+
+
+# Para Debian e derivados
+
+if which apt &>/dev/null; then
+
+sudo apt purge linphone -y
+sudo apt autoremove -y
+
+fi
+
+exit 0
