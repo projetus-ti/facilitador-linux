dad710f5af6864834d6ca41aa7bd498bc2aacc02
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-10-19 02:47:34 -0300

    Update DIRF.sh

--- atalhos/DIRF.sh
+++ atalhos/DIRF.sh
@@ -1,10 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Colaboração: Fernando Souza https://github.com/tuxslack / https://www.youtube.com/@fernandosuporte
+# Data: 19/10/2025
+
+# Usar array para pegar todos os possíveis caminhos
+
+JAVA_PATHS=(/usr/lib/jvm/*/bin/java)
+
+for JAVA in "${JAVA_PATHS[@]}"; do
+    if [[ -x "$JAVA" ]]; then
+        echo "Executável encontrado: $JAVA"
+        "$JAVA" -version
+    fi
+done
+
+
 cd "/opt/Programas RFB/Dirf2022"
+
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=ISO-8859-1
+
 umask 000
-if [ ! -e "/usr/lib/jvm/jre1.8.0_212/bin/java" ]; then
+
+if [ ! -e "$JAVA" ]; then
    java -jar pgdDirf.jar
   else
-    /usr/lib/jvm/jre1.8.0_212/bin/java -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
+    $JAVA -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
   fi
-#/usr/lib/jvm/jre1.8.0_212/bin/java -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
+  
+# $JAVA -jar -Xms512M -Xmx1024M -XX:+UseParallelOldGC pgdDirf.jar
