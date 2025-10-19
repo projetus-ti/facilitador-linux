1196a2861d2ea1969c9919132543f83fbe3d12e2
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-10-19 02:50:10 -0300

    Update efd-icms-ipi.sh

--- atalhos/efd-icms-ipi.sh
+++ atalhos/efd-icms-ipi.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# Colaboração: Fernando Souza https://github.com/tuxslack / https://www.youtube.com/@fernandosuporte
+# Data: 19/10/2025
 
 set echo off
 
-cd ~/ProgramasSPED/Fiscal/
+ls  ~/ProgramasSPED/ || exit
 
-~/ProgramasSPED/Fiscal/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/Fiscal/fiscalpva.jar
+cd ~/ProgramasSPED/Fiscal/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/Fiscal/fiscalpva.jar
+
+exit 0
