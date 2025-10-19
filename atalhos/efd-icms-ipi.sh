a3c79268affee5f355aac816bdfa87e9eb2914f6
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-10-19 03:13:40 -0300

    Update efd-icms-ipi.sh

--- atalhos/efd-icms-ipi.sh
+++ atalhos/efd-icms-ipi.sh
@@ -2,10 +2,23 @@
 # Colaboração: Fernando Souza https://github.com/tuxslack / https://www.youtube.com/@fernandosuporte
 # Data: 19/10/2025
 
-set echo off
 
-ls  ~/ProgramasSPED/ || exit
+# Verifica se o diretório existe
+if [[ ! -d ~/ProgramasSPED/ ]]; then
+    echo "Diretório ~/ProgramasSPED/ não encontrado."
+    exit 1
+fi
 
-cd ~/ProgramasSPED/Fiscal/jre/bin/java -Xmx2048m -Dfile.encoding=ISO-8859-1 -jar ~/ProgramasSPED/Fiscal/fiscalpva.jar
+# Verifica se o Java está presente
+if [[ ! -x ~/ProgramasSPED/Fiscal/jre/bin/java  ]]; then
+    echo "Java não encontrado em ~/ProgramasSPED/Fiscal/jre/bin/java"
+    exit 1
+fi
+
+# Executa
+~/ProgramasSPED/Fiscal/jre/bin/java  \
+    -Xmx2048m \
+    -Dfile.encoding=ISO-8859-1 \
+    -jar ~/ProgramasSPED/Fiscal/fiscalpva.jar
 
 exit 0
